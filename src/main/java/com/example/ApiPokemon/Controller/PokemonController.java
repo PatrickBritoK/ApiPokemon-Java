445d6c189ee7137ea21fbd531cbbcb335c170f26
Patrick Brito <patrickbritokun530@gmail.com>
2023-04-03 15:21:16 -0300

    att

--- src/main/java/com/example/ApiPokemon/Controller/PokemonController.java
+++ src/main/java/com/example/ApiPokemon/Controller/PokemonController.java
@@ -5,18 +5,20 @@ import java.util.Arrays;
 public class PokemonController {
     private final int id;
     private final String nome;
+    private final String regiao;
     private final String tipo;
     private final String habilidades;
     private final String sexo;
     private final String shiny;
 
-    public PokemonController(int id, String nome, String tipo, String[] habilidades, String sexo, String shiny) {
+    public PokemonController(int id, String nome, String tipo, String[] habilidades, String sexo, String shiny, String regiao) {
         this.id = id;
         this.nome = nome;
         this.tipo = tipo;
         this.habilidades = Arrays.toString(habilidades);
         this.sexo = sexo;
         this.shiny = shiny;
+        this.regiao = regiao;
     }
 
     public int getId() {
--- src/main/java/com/example/ApiPokemon/Model/PostPokemonModel.java
+++ src/main/java/com/example/ApiPokemon/Model/PostPokemonModel.java
@@ -47,7 +47,7 @@ public class PostPokemonModel {
                 lastId = pokemons.get(pokemons.size() - 1).getId();
             }
 
-            System.out.println("Arquivo lido com sucesso.");
+            System.out.println("###Adicione um Pokemon###");
         } catch (IOException ex) {
             System.out.println("Erro ao ler arquivo " + nomeArquivo + ": " + ex.getMessage());
         }
@@ -55,97 +55,98 @@ public class PostPokemonModel {
         int id = lastId + 1;
         //Nome do pokemon
         while (true) {
-                    System.out.println("Digite o nome do Pokémon:");
-                    String nome = scanner.nextLine();
-                    if (nome == null || nome.trim().isEmpty()) {
-                        System.out.println("Nome inválido. Por favor, digite novamente.");
-                        continue; // Volta para o início do loop
-                    }
-                    System.out.println("nome: " + nome);
-
-                    //Sexo do pokemon
-                    System.out.println("Digite M=Macho/F=Femea/D=Desconhecido \n Se a resposta for em branco vai ser automaticamente dada como Desconhecido.");
-                    String sexo = scanner.nextLine();
-                    if (sexo.equals("M") || sexo.equals("m")) {
-                        sexo = "Macho";
-                    } else if (sexo.equals("F") || sexo.equals("f")) {
-                        sexo = "Femea";
-                    } else if (sexo.equals("D") || sexo.equals("d")) {
-                        sexo = "Desconhecido";
-                    } else if (sexo.isEmpty()) {
-                        sexo = "Desconhecido";
-                    }
-                    System.out.println("sexo: " + sexo);
-                    //Tipo de pokemon
-                    String[] tipos = {"Normal", "Lutador", "Voador", "Venenoso", "Terra", "Pedra", "Inseto", "Fantasma", "Aço", "Fogo", "Água", "Planta", "Elétrico", "Psíquico", "Gelo", "Dragão", "Noturno", "Fada", "Desconhecido"};
-
-                    System.out.println("Escolha o tipo do Pokémon:");
-                    for (int i = 0; i < tipos.length; i++) {
-                        System.out.println((i + 1) + ". " + tipos[i]);
-                    }
-
-                    int escolhaTipo = scanner.nextInt();
-                    scanner.nextLine();
-
-                    String tipo;
-
-                    if (escolhaTipo < 1 || escolhaTipo > tipos.length) {
-                        System.out.println("Escolha inválida.");
-                        tipo = "desconhecido";
-                    } else {
-                        tipo = tipos[escolhaTipo - 1];
-                    }
-                    System.out.println("tipo: " + tipo);
-                    //Habilidades do pokemon
-                    System.out.println("O Pokémon possui uma ou duas habilidades? (1/2)");
-                    int numHabilidades = scanner.nextInt();
-                    scanner.nextLine();
-
-                    String[] habilidades = new String[numHabilidades];
-
-                    for (int i = 0; i < numHabilidades; i++) {
-                        System.out.println("Digite a habilidade #" + (i + 1) + " do Pokémon:");
-                        habilidades[i] = scanner.nextLine();
-                    }
-                    //Se o pokemon for shiny
-                    System.out.println("O Pokémon entra na categoria shiny?(s/n)");
-                    String shiny = scanner.nextLine();
-                    if (shiny.equals("s") || shiny.equals("S")) {
-                        shiny = "True";
-                    } else if (shiny.equals("n") || shiny.equals("N")) {
-                        shiny = "false";
-                    }
-
-                    // Cria um objeto Pokémon com as informações do usuário
-                    PokemonController pokemon = new PokemonController(id, nome, tipo, habilidades, sexo, shiny);
-
-                    id++;
-                    pokemons.add(pokemon);
-
-
-                    System.out.println("Deseja adicionar mais um Pokémon? (s/n)");
-                    String resposta = scanner.nextLine();
-
-                    if (resposta.equals("n")) {
-                        break;
-                    }
+            System.out.println("Digite o nome do Pokémon:");
+            String nome = scanner.nextLine();
+            if (nome == null || nome.trim().isEmpty()) {
+                System.out.println("Nome inválido. Por favor, digite novamente.");
+                continue; // Volta para o início do loop
             }
-            scanner.close();
+            System.out.println("nome: " + nome);
+
+            System.out.println("Qual a região?");
+            String regiao = scanner.nextLine();
 
 
-            // Cria um objeto Gson para serializar a lista de Pokémon em formato JSON
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            String json = gson.toJson(pokemons);
-
-            try {
-                FileWriter fileWriter = new FileWriter(nomeArquivo);
-                fileWriter.write(json);
-                fileWriter.close();
-                System.out.println("Pokemon registrado com sucesso.");
-            } catch (IOException ex) {
-                System.out.println("Erro ao gravar arquivo " + nomeArquivo + ": " + ex.getMessage());
+            //Sexo do pokemon
+            System.out.println("Digite M=Macho/F=Femea/D=Desconhecido \n Se a resposta for em branco vai ser automaticamente dada como Desconhecido.");
+            String sexo = scanner.nextLine();
+            if (sexo.equals("M") || sexo.equals("m")) {
+                sexo = "Macho";
+            } else if (sexo.equals("F") || sexo.equals("f")) {
+                sexo = "Femea";
+            } else if (sexo.equals("D") || sexo.equals("d")) {
+                sexo = "Desconhecido";
+            } else if (sexo.isEmpty()) {
+                sexo = "Desconhecido";
             }
-        }
+            System.out.println("sexo: " + sexo);
 
-    }
+            //Tipo de pokemon
+            String[] tipos = {"Normal", "Lutador", "Voador", "Venenoso", "Terra", "Pedra", "Inseto", "Fantasma", "Aço", "Fogo", "Água", "Planta", "Elétrico", "Psíquico", "Gelo", "Dragão", "Noturno", "Fada", "Desconhecido"};
+
+            System.out.println("Escolha o tipo do Pokémon:");
+            for (int i = 0; i < tipos.length; i++) {
+                System.out.println((i + 1) + ". " + tipos[i]);
+            }
+
+            int escolhaTipo = scanner.nextInt();
+            scanner.nextLine();
+
+            String tipo;
+
+            if (escolhaTipo < 1 || escolhaTipo > tipos.length) {
+                System.out.println("Escolha inválida.");
+                tipo = "desconhecido";
+            } else {
+                tipo = tipos[escolhaTipo - 1];
+            }
+            System.out.println("tipo: " + tipo);
+            //Habilidades do pokemon
+            System.out.println("O Pokémon possui uma ou duas habilidades? (1/2)");
+            int numHabilidades = scanner.nextInt();
+            scanner.nextLine();
+
+            String[] habilidades = new String[numHabilidades];
+
+            for (int i = 0; i < numHabilidades; i++) {
+                System.out.println("Digite a habilidade #" + (i + 1) + " do Pokémon:");
+                habilidades[i] = scanner.nextLine();
+            }
+            //Se o pokemon for shiny
+            System.out.println("O Pokémon entra na categoria shiny?(s/n)");
+            String shiny = scanner.nextLine();
+            if (shiny.equals("s") || shiny.equals("S")) {
+                shiny = "True";
+            } else if (shiny.equals("n") || shiny.equals("N")) {
+                shiny = "false";
+            }
+
+            // Cria um objeto Pokémon com as informações do usuário
+            PokemonController pokemon = new PokemonController(id, nome, tipo, habilidades, sexo, shiny, regiao);
+
+            id++;
+            pokemons.add(pokemon);
+
+
+            System.out.println("Deseja adicionar mais um Pokémon? (s/n)");
+            String resposta = scanner.nextLine();
+
+            if (resposta.equals("n")) {
+                break;
+            }}
+        scanner.close();
+
+
+        // Cria um objeto Gson para serializar a lista de Pokémon em formato JSON
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(pokemons);
+
+        try {
+            FileWriter fileWriter = new FileWriter(nomeArquivo);
+            fileWriter.write(json);
+            fileWriter.close();
+            System.out.println("Pokemon registrado com sucesso.");
+        } catch (IOException ex) {
+            System.out.println("Erro ao gravar arquivo " + nomeArquivo + ": " + ex.getMessage());
+        }}}
 
